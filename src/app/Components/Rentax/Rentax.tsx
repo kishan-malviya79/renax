a0e443db85362bb7c5acabdac2202cc4bbac3b59
kishan0779 <124876418+kishan0779@users.noreply.github.com>
2024-05-18 21:34:06 +0530

    s

--- src/app/Components/Rentax/Rentax.tsx
+++ src/app/Components/Rentax/Rentax.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/components/ui/button'
-import { ArrowUpRight, CheckCircle2, LucideCheckCircle2 } from 'lucide-react'
+import { ArrowUpRight, CheckCircle2, LucideCheck, LucideCheckCircle2, Play } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
 
@@ -14,11 +14,11 @@ function Rentax() {
         <h1 className=' text-[35px] leading-5 text-[#f5b754] font-bold '>A Car Rental Company </h1> 
             <p className='w-[70ch] text-xs opacity-50 leading-7 '>Car repair quisque sodales dui ut varius vestibulum drana tortor turpis porttiton tellus eu euismod nisl massa nutodio in the miss volume place urna lacinia eros nunta urna mauris vehicula rutrum in the miss on volume interdum.  </p>
         <div className="flex flex-col gap-4">
-            <h1 className=' flex items-center '>
-            <LucideCheckCircle2 className="mr-2 h-8 w-8  border-black border rounded-full " />
+            <h1 className=' flex opacity-50 items-center '>
+            <LucideCheck className="mr-3 h-8 w-4 text-[#f5b754]  " />
                 Sports and Luxury Cars</h1>
-            <h1 className=' flex items-center '>
-            <LucideCheckCircle2 className="mr-2 h-8 w-8  border-black border rounded-full " />
+            <h1 className=' flex opacity-50 items-center '>
+            <LucideCheck className="mr-3  h-8 w-4 text-[#f5b754]   " />
             Economy Cars</h1>
           
         </div>
@@ -29,14 +29,22 @@ function Rentax() {
             </Button>
         </div>
     </div>
-    <div className="flex">
+    <div className="flex  relative">
     <Image
           src={`/about.jpg`}
           alt="jordans"
           height="500"
           width="500"
-          className="object-contain rounded-[24px]  "
+          className="object-contain rounded-tl-3xl rounded-tr-3xl  rounded-bl-none rounded-br-3xl transition-transform duration-300 transform hover:scale-110  "
         />
+        <div className=" absolute bottom-0 left-0 bg-black w-[85px] h-[85px] text-center rounded-tl-none rounded-tr-lg">
+    
+          <Button className='absolute top-[15px] left-0  h-8 px-6 py-6 bg-transparent border-[#f5b754] border hover:bg-[#f5b754] rounded-[50px]   '>
+          <Play className="text-white" />
+            </Button>
+        </div>
+        <div className="absolute top-[-24px] left-[-1px]  leading-[1]  "></div>
+        <div className="absolute bottom-[-1px] right-[-24px]  leading-[1] "></div>
     </div>
     </div>
     </div>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,12 +20,18 @@ const buttonVariants = cva(
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
-      size: {
-        default: "h-9 px-12 py-7 bg-[#f5b754] rounded-[50px]",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
-        icon: "h-9 w-9",
-      },
+        size: {
+          default: "h-9 px-12 py-7 bg-[#f5b754] rounded-[50px]",
+          sm: "h-8 rounded-md px-3 text-xs",
+          lg: "h-10 rounded-md px-8",
+          icon: "h-9 w-9",
+        },
+        size1: {
+          default: "h-9 px-6 py-6 bg-[#f5b754] rounded-[50px]",
+          sm: "h-8 rounded-md px-3 text-xs",
+          lg: "h-10 rounded-md px-8",
+          icon: "h-9 w-9",
+        },
     },
     defaultVariants: {
       variant: "default",
