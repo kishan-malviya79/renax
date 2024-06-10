06659addf7068cdcb22b1ec79fc84123a01c353c
kishan0779 <124876418+kishan0779@users.noreply.github.com>
2024-06-11 03:02:14 +0530

    all set

--- src/app/Components/LuxuryCar/LuxuryCar.tsx
+++ src/app/Components/LuxuryCar/LuxuryCar.tsx
@@ -22,7 +22,7 @@ const LuxuryCar: React.FC = () => {
     <div className="bg-red-600 text-white overflow-hidden w-full">
       <div className="w-full  md:h-screen bg-[#1b1b1b]">
         <div className="w-full pt-28 pb-12 sm:py-28">
-          <div className="relative items-center justify-center text-white flex sm:px-36 w-full md:w-[1440px] mx-auto md:py-0">
+          <div className="relative items-center justify-center text-white flex lg:px-36 w-full lg:w-[1440px] mx-auto md:py-0">
             <div className="items-center flex justify-center flex-col gap-4">
               <h5 className="size-5 flex items-center justify-center sm:right-3 w-full text-[#f5b754] tracking-[6px] text-[12px]">SELECT YOUR CAR</h5>
               <h1 className="font-Outfit font-bold w-full text-[35px]">Luxury<span className="text-[#f5b754]"> Car Fleet</span></h1>
@@ -32,15 +32,15 @@ const LuxuryCar: React.FC = () => {
         <Carousel>
           <CarouselContent className="gap-6">
             {carDetails.map((car, index) => (
-              <CarouselItem key={index} className="md:basis-1/2 text-white lg:basis-1/3">
+              <CarouselItem key={index} className="md:basis-1/1 relative md:px-20 lg:px-0 text-white lg:basis-1/3">
                 <Image
                   src={car.src}
                   alt={car.name}
                   height={1100}
                   width={1100}
-                  className="object-contain relative left-2 sm:left-0 rounded-2xl w-[360px]  sm:w-full"
+                  className="object-contain relative left-2 lg:left-0 rounded-2xl  w-[360px]  md:w-[720px] lg:w-full"
                 />
-                <div className="relative -top-7 px-4 py-6 rounded-2xl sm:items-center flex sm:flex-row flex-col justify-between gap-3 sm:gap-0 bg-[#222] mx-3">
+                <div className="relative -top-7 px-4 py-6 rounded-2xl sm:items-center flex sm:flex-row flex-col  lg:justify-between gap-3 md:gap-36 lg:gap-0 bg-[#222] mx-3">
                   <div className="flex flex-col">
                     <h1 className="font-Outfit font-bold text-[18px]">{car.name}</h1>
                     <div className="flex  gap-2 sm:gap-4 text-sm items-center opacity-60">
--- src/app/Components/Rentax/Rentax.tsx
+++ src/app/Components/Rentax/Rentax.tsx
@@ -6,19 +6,16 @@ import React from 'react'
 function Rentax() {
   return (
     <div className="bg-[#1b1b1b] py-12 md:py-28">
-      <div className="container mx-auto px-6 md:px-36 md:max-w-[1440px] flex flex-col md:flex-row items-center justify-between text-white">
+      <div className="container md:px-20 px-6 lg:px-36 lg:w-[1440px] md:gap-8 flex flex-col lg:flex-row md:items-start items-center justify-between text-white">
 
         {/* <div className=' relative items-center  justify-between  content-center  text-white flex   px-36 w-full md:w-[1440px] mx-auto md:py-0'> */}
-        <div className=" flex flex-col gap-4">
+        <div className=" flex flex-col w-full gap-4">
           <h5 className="text-[#f5b754] tracking-[6px] text-[12px]">RENTAX</h5>
-          <h1 className="font-Outfit font-bold text-3xl md:text-4xl lg:text-5xl">We Are More Than</h1>
+          <h1 className="font-Outfit font-bold  text-3xl md:text-4xl lg:text-5xl">We Are More Than</h1>
           <h1 className="text-3xl md:text-4xl lg:text-5xl leading-8 text-[#f5b754] font-bold">A Car Rental Company</h1>
-          <p className="max-w-prose text-xs md:text-sm opacity-50 leading-7">Car repair quisque sodales dui ut varius vestibulum drana tortor turpis porttiton tellus eu euismod nisl massa nutodio in the miss volume place urna lacinia eros nunta urna mauris vehicula rutrum in the miss on volume interdum. </p>
+          <p className="max-w-prose text-xs md:w-[95ch] lg:text-sm opacity-50 leading-7">Car repair quisque sodales dui ut varius vestibulum drana tortor turpis porttiton tellus eu euismod nisl massa nutodio in the miss volume place urna lacinia eros nunta urna mauris vehicula rutrum in the miss on volume interdum. </p>
           <div className="flex flex-col gap-4">
-            <h1 className="flex items-center opacity-50 text-sm">
-              <LucideCheck className="mr-3 h-8 w-4 text-[#f5b754]" />
-              Sports and Luxury Cars
-            </h1>
+            
             <h1 className=' flex opacity-50 items-center '>
               <LucideCheck className="mr-3 h-8 w-4 text-[#f5b754]  " />
               Sports and Luxury Cars</h1>
@@ -40,7 +37,7 @@ function Rentax() {
             alt="jordans"
             height="500"
             width="500"
-            className="object-contain rounded-tl-3xl rounded-tr-3xl  rounded-bl-none rounded-br-3xl transition-transform duration-300 transform hover:-scale-x-100 "
+            className="object-contain rounded-tl-3xl rounded-tr-3xl w-[360px]  md:w-[720px] lg:w-[500px] rounded-bl-none rounded-br-3xl transition-transform duration-300 transform hover:-scale-x-100 "
           />
           <div className=" absolute bottom-0 left-0 bg-[#1b1b1b] w-[85px] h-[85px] text-center rounded-tl-none rounded-tr-lg">
 
